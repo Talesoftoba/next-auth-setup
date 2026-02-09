243bad63a0c7dc6f4b792e14d737ca0b14c83a7c
Talesoftoba <Tobaking4@gmail.com>
2026-02-09 14:10:11 +0100

    v44

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,12 @@
-"use client";
 
+"use client";
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
+
+
+
+
+
 
 import { signIn } from "next-auth/react";
 import { useState } from "react";
